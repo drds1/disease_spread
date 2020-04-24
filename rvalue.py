9523e0c17bc399d64739affe4153c4c27be96338
dstarkey23 <david.starkey@vortexa.com>
2020-04-24 18:30:57 +0100

    rvalue plot changes

--- rvalue.py
+++ rvalue.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
     ax1.set_xlabel('')
-    ax1.set_ylabel('Infected percentage reduction')
-    ax1.set_title('Infected fraction reduction timescale')
+    ax1.set_ylabel('Remaining Infected Population\n(% relative to today)')
+    ax1.set_title('Infection reduction timescale\n Dependence on "r"')
     #compute the infected fraction (relative to current levels) vs time
     for rnow in r:
         f = 100*rnow**(t/tau)
@@ -34,7 +34,7 @@ if __name__ == '__main__':
             idx_annotation_fraction = np.where(f < annotation_fraction)[0][0]
             date_af = dates[idx_annotation_fraction]
             weeks_away = int(np.round(pd.Timedelta(date_af - pd.Timestamp(today)).days/7))
-            txt = str(int(annotation_fraction*100))+'% reduction in ' + str(weeks_away) +' weeks\n'
+            txt = str(int(annotation_fraction))+'% reduction in ' + str(weeks_away) +' weeks\n'
             ax1.plot(dates, f, label='r = ' + str(np.round(rnow, 2))+'\n'+txt, color=color)
             ax1.plot([date_af] * 2, [0, annotation_fraction], label=None, ls=':', color=color)
         else:
