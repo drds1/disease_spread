fa8296749aeb6fa435be6a9a4c1a5140012610d8
dstarkey23 <david.starkey@vortexa.com>
2021-01-25 17:41:00 +0000

    add pydlm example

--- scratch_dynamic_lr_example.py
+++ scratch_dynamic_lr_example.py
@@ -1,9 +1,48 @@
-from pandas_datareader import DataReader
+#from pandas_datareader import DataReader
 from datetime import datetime
 import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
-import pyflux as pf
+#import pyflux as pf
+
+from pydlm import dlm, trend, seasonality, dynamic, autoReg, longSeason
+data = np.array([0] * 100 + [3] * 100)
+myDLM = dlm(data)
+myDLM = myDLM + trend(degree=1, discount=0.95, name='trend1')
+myDLM.fit()
+
+coef = np.array(myDLM.getLatentState())
+results = np.array(myDLM.result.predictedObs)[:,0,0]
+results_var = np.array(myDLM.result.predictedObsVar )[:,0,0]
+
+
+import matplotlib.pylab as plt
+#fig = plt.figure()
+#ax1 = fig.add_subplot(311)
+#ax1.plot(coef[:,0])
+#ax2 = fig.add_subplot(312)
+#ax2.plot(coef[:,1])
+#ax3 = fig.add_subplot(313)
+#ax3.plot(results)
+#plt.show()
+
+
+
+'''
+
+
+import pydlm
+#data = np.random.random((1, 1000))
+#myDlm = pydlm.dlm(data) + pydlm.trend(degree = 2, discount = 0.98)
+#myDlm.fitForwardFilter()
+
+data = np.random.random((1,100))
+mydlm = pydlm.dlm(data) + pydlm.trend(degree=1, discount = 0.98, name='a') \
+        + pydlm.dynamic(features=[[i] for i in range(100)], discount = 1, name='b')
+mydlm.fit()
+coef_a = mydlm.getLatentState('a')
+coef_b = mydlm.getLatentState('b')
+
 
 a = DataReader('AMZN',  'yahoo', datetime(2012,1,1), datetime(2016,6,1))
 a_returns = pd.DataFrame(np.diff(np.log(a['Adj Close'].values)))
@@ -37,5 +76,6 @@ x = model.fit()
 x.summary()
 
 model.plot_fit()
+'''
 
 
