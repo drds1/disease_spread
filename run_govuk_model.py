efa3e316187147755ab6ec9814804f30aa961357
dstarkey23 <david.starkey@vortexa.com>
2021-01-31 16:26:08 +0000

    Results 2021-01-31

--- run_govuk_model.py
+++ run_govuk_model.py
@@ -1,3 +1,13 @@
 from rvalue_model import *
+import subprocess as cmd
+import pandas as pd
 
 x = run_govukmodel()
+
+
+today_str = str(pd.Timestamp.today().date())
+#commit results to github
+cp = cmd.run("git add .", check=True, shell=True)
+message = "Results "+today_str
+cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
+cp = cmd.run("git push -u origin master -f", check=True, shell=True)
