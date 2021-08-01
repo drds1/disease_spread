7da23b3c783373865be6e68cd12bcaf75772be2d
dstarkey23 <davidstarkey29@gmail.com>
2021-08-01 16:36:06 +0100

    add DOcker updates

--- Docker/build_docker.sh
+++ Docker/build_docker.sh
@@ -1,2 +1,13 @@
-docker build -f docker/Dockerfile -t test_container .
-docker run --rm -it -v test_container bash
+#docker build -f Docker/Dockerfile -t test_container .
+
+docker build -f Docker/Dockerfile --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa.pub)" -t test_container .
+
+
+#docker run --rm -it -v test_container bash
+docker run -it test_container bash
+#docker run --rm -it test_container bash
+#docker attach test_container
+
+#delete all containers and images
+#docker rm -vf $(docker ps -a -q)
+#docker rmi -f $(docker images -a -q)
